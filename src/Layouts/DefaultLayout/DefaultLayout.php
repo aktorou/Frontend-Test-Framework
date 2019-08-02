f794a1e6007fb115c67762309a32cd3920adfd85
Ashley Ktorou <ashley.ktorou@justdevelop.it>
2019-08-02 11:58:13 +0100

    remove loading resources from default layout in framework

--- src/Layouts/DefaultLayout/DefaultLayout.php
+++ src/Layouts/DefaultLayout/DefaultLayout.php
@@ -1,28 +1,16 @@
 <?php
 namespace ProtectedNet\FrontendTestFramework\Layouts\DefaultLayout;
 
-use Packaged\Dispatch\ResourceManager;
 use ProtectedNet\FrontendTestFramework\Layouts\AbstractLayout;
 use ProtectedNet\FrontendTestFramework\Layouts\Interfaces\FooterLayout;
 use ProtectedNet\FrontendTestFramework\Layouts\Interfaces\HeroLayout;
 use ProtectedNet\FrontendTestFramework\Layouts\Interfaces\NavLayout;
-use ProtectedNet\FrontendTestFramework\Partials\Footer\Footer;
-use ProtectedNet\FrontendTestFramework\Partials\Nav\Nav;
-use ProtectedNet\TavBranding\TavBranding;
-use ProtectedNet\UiComponents\Core\Link\Link;
-use ProtectedNet\UiComponents\Core\Modal\Modal;
 
 class DefaultLayout extends AbstractLayout implements NavLayout, HeroLayout, FooterLayout
 {
   protected $_nav = false;
   protected $_footer = false;
   protected $_hero = false;
-
-  public function __construct()
-  {
-    ResourceManager::resources()->includeCss('styles/global.min.css', null, 20);
-    ResourceManager::resources()->includeJs('js/libraries.min.js', [], 20);
-  }
 
   /** @return mixed|null */
   protected function _getHero()
